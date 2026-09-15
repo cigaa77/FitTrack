da6597664d6314f4eab6e0eed48561d1dd9892ee
Ahmet Cilingir <face4forev@icloud.com>
2026-09-15 17:57:42 +0200

    Initial Commit

---
+++ FitTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitTrack
+//
+//  Created by Ahmet CILINGIR on 15.09.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
